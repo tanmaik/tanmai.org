33f20f35eb759e357b7ee0c7431b9ea2bd4eec46
Tanmai Kalisipudi <tanmai.kalisipudi@gmail.com>
2024-06-18 09:46:44 -0400

    Add GradSteps and UVA

--- app/page.js
+++ app/page.js
@@ -20,8 +20,8 @@ const Expandable = ({ header, description }) => {
 
 export default function Home() {
   return (
-    <div className="text-sm text-primary">
-      <div className="px-4 sm:w-[25rem] w-full">
+    <div className="text-sm text-primary flex justify-center sm:mt-20">
+      <div className="px-4 sm:w-[30rem] w-full">
         <div className="flex justify-between mt-4 ">
           <div>
             <h1>tanmai kalisipudi</h1>
@@ -71,6 +71,9 @@ export default function Home() {
 
         <h1 className="mt-8">experience</h1>
 
+        <h1 className="text-secondary mt-2">now</h1>
+        <Expandable header="ceo @ GradSteps" description="gradsteps.com" />
+
         <h1 className="text-secondary mt-2">summer 2023</h1>
 
         <Expandable
@@ -93,19 +96,12 @@ export default function Home() {
         <h1 className="mt-8">education</h1>
 
         <h1 className="text-secondary mt-2">in progress until 2026</h1>
-        <h1>university of pittsburgh, b.s. computer science</h1>
+        <h1>university of virginia, computer science</h1>
         <h1 className="text-secondary mt-2">graduated in 2023</h1>
         <h1>thomas jefferson high school for science and technology</h1>
 
         <footer className="mt-20 text-xs text-secondary">
-          <a
-            href="https://www.gradsteps.com/"
-            target="_blank"
-            className="italic underline"
-          >
-            looking for our company home?
-          </a>
-          <p>©2024 Tanmai Kalisipudi. All rights reserved.</p>
+          <p>©2024 tanmai kalisipudi. all rights reserved.</p>
         </footer>
       </div>
     </div>
