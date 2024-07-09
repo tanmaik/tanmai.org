d1a68d9cfa19708bd3bba0b01739c05044a30aa3
Tanmai Kalisipudi <tanmai.kalisipudi@gmail.com>
2024-07-09 14:18:39 -0400

    First part of GOOEY

--- app/gooey/actions.tsx
+++ app/gooey/actions.tsx
@@ -1,7 +1,21 @@
 "use server";
 
-import { streamUI } from "ai/rsc";
-import { anthropic } from "@ai-sdk/anthropic";
-import {z} from 'zod'
+import { streamText } from "ai";
+import { openai } from "@ai-sdk/openai";
+import { z } from "zod";
 
-const 
+export async function streamComponent(input: string) {
+  const { textStream } = await streamText({
+    model: openai("gpt-4o"),
+    prompt: input,
+    system:
+      "Generate raw HTML with inline styling to render a component; use raw HTML and no imports; style as nicely as you can. Generate just the stuff inside the <body> tag, and don't include any extra commentary. \n Example input: A card that says hello. \n Example output: <div>Hello!</div>",
+  });
+
+  let stream = "";
+  for await (const textPart of textStream) {
+    stream += textPart;
+    console.log(textPart)
+  }
+  return stream;
+}
--- app/gooey/page.tsx
+++ app/gooey/page.tsx
@@ -1,20 +1,34 @@
 "use client";
-import { useState, useEffect } from "react";
+import { useState } from "react";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { streamComponent } from "./actions";
+import DynamicComponent from "./DynamicComponent";
 
 export default function Page() {
-  const [mounted, setMounted] = useState<boolean>(false);
-
-  useEffect(() => {
-    setMounted(true);
-  }, []);
-
-  if (!mounted) return null;
+  const [component, setComponent] = useState<string>("");
 
   return (
-    <div
-      dangerouslySetInnerHTML={{
-        __html: "",
-      }}
-    />
+    <div>
+      <form
+        onSubmit={async (e) => {
+          e.preventDefault();
+          const result = await streamComponent(e.currentTarget.input.value);
+          console.log(result);
+
+          setComponent(result);
+        }}
+      >
+        <Input type="text" name="input" />
+        <Button>Submit</Button>
+      </form>
+      <div>
+        {component.length > 0 && (
+          <>
+            <DynamicComponent html={component} />
+          </>
+        )}
+      </div>
+    </div>
   );
 }
