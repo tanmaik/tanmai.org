ebaf17bdccf80e850a183abd6a796b1fe3a5ca07
Tanmai Kalisipudi <tanmai.kalisipudi@gmail.com>
2024-10-11 13:29:31 -0400

    change website

--- app/page.js
+++ app/page.js
@@ -1,41 +1,35 @@
 export default function Home() {
   return (
-    <div className="p-4">
-      <h1 className="font-bold">tanmai</h1>
-      <p>
-        find me on{" "}
-        <a
-          href="https://www.linkedin.com/in/tanmaikalisipudi"
-          className="underline"
-        >
-          linkedin
-        </a>{" "}
-        /{" "}
-        <a href="https://github.com/tanmaik" className="underline">
-          github
-        </a>{" "}
-        /{" "}
-        <a href="https://x.com/nottanmai" className="underline">
-          x
-        </a>
-      </p>
-      <p>
-        or email{" "}
-        <a href="mailto:tanmai.kalisipudi@gmail.com" className="underline">
-          tanmai.kalisipudi@gmail.com
-        </a>
-      </p>
-      <p className="mt-2 flex flex-col">
-        <a
-          className="underline font-semibold"
-          href="https://buy.stripe.com/00g15MbGEfeTdP2bIK"
-        >
-          buy my ai product
-        </a>
-        <a className=" cursor-not-allowed font-semibold text-gray-500">
-          browse addons
-        </a>
-      </p>
+    <div className="flex justify-center">
+      <div className="max-w-lg px-4">
+        <h1 className="mt-20">tanmai</h1>
+        <p className="mt-4">
+          im a cs student at the university of virginia, and also the founder of
+          a company called perceptron labs.{" "}
+        </p>
+        <p className="mt-4">
+          you can find me on{" "}
+          <a className="underline" href="https://github.com/tanmaik">
+            github
+          </a>
+          ,{" "}
+          <a
+            className="underline"
+            href="https://www.linkedin.com/in/tanmaikalisipudi/"
+          >
+            linkedin
+          </a>
+          ,{" "}
+          <a className="underline" href="https://twitter.com/nottanmai">
+            x
+          </a>
+          , and{" "}
+          <a className="underline" href="mailto:tanmai.kalisipudi@gmail.com">
+            email
+          </a>
+          .
+        </p>
+      </div>
     </div>
   );
 }
