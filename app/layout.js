8150bd644d6d3be70fe7e75f7e10ac3ccc773882
Tanmai Kalisipudi <tanmai.kalisipudi@gmail.com>
2024-10-11 13:44:21 -0400

    redirect all traffic to github

--- app/layout.js
+++ app/layout.js
@@ -7,7 +7,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="">{children}</body>
+      <body>{children}</body>
     </html>
   );
 }
--- app/page.js
+++ app/page.js
@@ -1,35 +1,5 @@
+import { redirect } from "next/navigation";
+
 export default function Home() {
-  return (
-    <div className="flex justify-center">
-      <div className="max-w-lg px-4">
-        <h1 className="mt-20">tanmai</h1>
-        <p className="mt-4">
-          im a cs student at the university of virginia, and also the founder of
-          a company called perceptron labs.{" "}
-        </p>
-        <p className="mt-4">
-          you can find me on{" "}
-          <a className="underline" href="https://github.com/tanmaik">
-            github
-          </a>
-          ,{" "}
-          <a
-            className="underline"
-            href="https://www.linkedin.com/in/tanmaikalisipudi/"
-          >
-            linkedin
-          </a>
-          ,{" "}
-          <a className="underline" href="https://twitter.com/nottanmai">
-            x
-          </a>
-          , and{" "}
-          <a className="underline" href="mailto:tanmai.kalisipudi@gmail.com">
-            email
-          </a>
-          .
-        </p>
-      </div>
-    </div>
-  );
+  redirect("https://github.com/tanmaik");
 }
