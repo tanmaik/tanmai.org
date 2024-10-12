e9237dd2d8deef05aeec044cd564f335ec2b5b73
Tanmai Kalisipudi <tanmai.kalisipudi@gmail.com>
2024-10-12 03:22:34 -0400

    redirects

---
+++ app/friday/page.js
@@ -0,0 +1,5 @@
+import { redirect } from "next/navigation";
+
+export default function Friday() {
+  redirect("https://friday-beta.vercel.app");
+}
--- next.config.mjs
+++ next.config.mjs
@@ -8,18 +8,6 @@ const nextConfig = {
       },
     ],
   },
-  async rewrites() {
-    return [
-      {
-        source: "/",
-        destination: "https://github.com/tanmaik",
-      },
-      {
-        source: "/friday",
-        destination: "https://friday-beta.vercel.app",
-      },
-    ];
-  },
 };
 
 export default nextConfig;
