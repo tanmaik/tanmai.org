379dcc4450dfefbc9dd3ed2265bfaf9f505305af
Tanmai Kalisipudi <tanmai.kalisipudi@gmail.com>
2025-01-13 18:42:55 -0500

    fix website

--- app/page.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <main className="p-4">
-      <p>tanmai kalisipudi</p>
-      <p>tanmai.kalisipudi@gmail.com</p>
-    </main>
-  );
-}
---
+++ app/page.tsx
@@ -0,0 +1,35 @@
+export default function Home() {
+  return (
+    <div className="p-2">
+      <p>tanmai kalisipudi</p>
+      <p>tanmai.kalisipudi@gmail.com</p>
+
+      <div className="flex gap-1 items-center">
+        <a
+          className="underline"
+          href="https://x.com/nottanmai"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          x
+        </a>
+        <a
+          className="underline"
+          href="https://github.com/tanmaik"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          github
+        </a>
+        <a
+          className="underline"
+          href="https://linkedin.com/in/tanmaikalisipudi"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          linkedin
+        </a>
+      </div>
+    </div>
+  );
+}
