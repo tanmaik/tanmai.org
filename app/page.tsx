ccf386d03658891f0f8950d7eb311c7cbcb30b1f
Tanmai Kalisipudi <tanmai.kalisipudi@gmail.com>
2025-02-07 12:47:36 -0500

    more personal

--- app/page.tsx
+++ app/page.tsx
@@ -1,34 +1,36 @@
 export default function Home() {
   return (
-    <div className="p-2">
-      <p>tanmai kalisipudi</p>
-      <p>tanmai.kalisipudi@gmail.com</p>
+    <div className="flex justify-center py-10">
+      <div className="w-full max-w-xl">
+        <p className="font-bold text-2xl">tanmai kalisipudi</p>
+        <p>tanmai.kalisipudi@gmail.com</p>
 
-      <div className="flex gap-1 items-center">
-        <a
-          className="underline"
-          href="https://x.com/nottanmai"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          x
-        </a>
-        <a
-          className="underline"
-          href="https://github.com/tanmaik"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          github
-        </a>
-        <a
-          className="underline"
-          href="https://linkedin.com/in/tanmaikalisipudi"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          linkedin
-        </a>
+        <div className="flex gap-1 items-center">
+          <a
+            className="underline"
+            href="https://x.com/nottanmai"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            x
+          </a>
+          <a
+            className="underline"
+            href="https://github.com/tanmaik"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            github
+          </a>
+          <a
+            className="underline"
+            href="https://linkedin.com/in/tanmaikalisipudi"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            linkedin
+          </a>
+        </div>
       </div>
     </div>
   );
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,21 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**"
+      },
+      {
+        protocol: "http",
+        hostname: "**",
+      },
+    ],
+  },
+  devIndicators: {
+    appIsrStatus: false
+  }
 };
 
 export default nextConfig;
