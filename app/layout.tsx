59da293f4b37617a36adef7c4821041be48b25df
Tanmai Kalisipudi <tanmai.kalisipudi@gmail.com>
2025-04-04 14:35:07 -0400

    change base website

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,8 @@
 import "./globals.css";
-import { Analytics } from "@vercel/analytics/react";
 import type { Metadata } from "next";
 
-import localFont from "next/font/local";
-
-const favorit = localFont({
-  src: "./fonts/Favorit.ttf",
-});
-
 export const metadata: Metadata = {
-  title: "tanmai",
+  title: "tanmai kalisipudi",
 };
 
 export default function RootLayout({
@@ -19,12 +12,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={favorit.className}>
-        <main className="flex justify-center underline-offset-2 py-10">
-          <div className="w-full max-w-2xl px-4">{children}</div>
-        </main>
-        <Analytics />
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,45 +1,34 @@
-import Socials from "./components/nav";
-import ImportantStuff from "./components/important-stuff";
-import Writing from "./components/writing";
-import RecentlyPlayed from "./components/recently-played";
-import RecentActivity from "./components/recent-activity";
-import LocationFlipper from "./components/location-flipper";
-import CodingActivity from "./components/coding-activity";
-import CodingLanguages from "./components/coding-languages";
-
 export default function Home() {
   return (
-    <div>
-      <div className="flex items-end justify-between">
-        <div>
-          <LocationFlipper />
-          <p className="font-semibold text-lg sm:text-2xl">
-            hi, i&apos;m <span className="font-bold">tanmai kalisipudi</span>
+    <>
+      <main className="flex items-center justify-center">
+        <div className="w-full px-4 max-w-xl">
+          <h1 className="text-2xl font-bold mt-8">hey i&apos;m tanmai</h1>
+          <p className="mt-4">
+            currently, im a cs+math major at the university of virginia. i am
+            mainly interested in ai, startups, economics, and politics.
+          </p>
+          <h2 className="text-xl font-bold mt-8">previously</h2>
+          <p className="mt-4">
+            i was a software engineering intern at lineaje where i spearheaded
+            their flagship agentic llm product and developed tools using
+            langchain and fine-tuned llms to improve enterprise customer
+            compliance.
+          </p>
+          <p className="mt-4">
+            before that, i interned at cisco systems where i worked on
+            optimizing shipping crate storage during the covid supply chain
+            crisis using cisco meraki and nexus dashboard.
+          </p>
+          <p className="mt-4">
+            i&apos;ve also worked on several projects including gradsteps (an
+            automated degree planner), tjrides (a carpool platform for high
+            school students), and electralert (a drowsy driving detection system
+            using eeg). these projects involved technologies like aws, react,
+            next.js, and machine learning.
           </p>
         </div>
-        <Socials />
-      </div>
-      <hr className="my-3" />
-
-      <p>
-        currently, im a cs+math major at the university of virginia. i am mainly
-        interested in ai, startups, economics, and politics.
-      </p>
-
-      <p className="mt-2">
-        some more stuff ive been up to recently: attempting a sub-3hr marathon,
-        learning how to play the piano, and dabbling in design / writing. i plan
-        to share more about these things here.
-      </p>
-
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-6 my-6">
-        <RecentlyPlayed />
-        <RecentActivity />
-        <CodingActivity />
-        <CodingLanguages />
-      </div>
-      <ImportantStuff />
-      <Writing />
-    </div>
+      </main>
+    </>
   );
 }
