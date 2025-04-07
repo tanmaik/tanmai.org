a6828ace8ed066d9d97ce1c7b25b595423564fa7
Tanmai Kalisipudi <tanmai.kalisipudi@gmail.com>
2025-04-07 18:00:10 -0400

    finish links

--- app/page.tsx
+++ app/page.tsx
@@ -6,11 +6,30 @@ export default function Home() {
           <h1 className="text-2xl font-bold mt-8">hey i&apos;m tanmai</h1>
           <p className="mt-4">
             currently, im a cs+math major at the university of virginia. i am
-            mainly interested in ai, startups, economics, and politics.
+            mainly interested in ai, startups, economics, and politics. you can
+            find me on{" "}
+            <a
+              href="https://github.com/tanmaik"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-blue-600 hover:underline"
+            >
+              github
+            </a>{" "}
+            and{" "}
+            <a
+              href="https://linkedin.com/in/tanmaikalisipudi"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-blue-600 hover:underline"
+            >
+              linkedin
+            </a>
+            .
           </p>
           <h2 className="text-xl font-bold mt-8">previously</h2>
           <p className="mt-4">
-            in 2024, i was a software engineering intern at lineaje where i
+            in 2023, i was a software engineering intern at lineaje where i
             spearheaded their flagship agentic llm product using langchain and
             fine-tuned llms to improve instruction following.
           </p>
@@ -25,12 +44,22 @@ export default function Home() {
               href="https://gradsteps.com"
               target="_blank"
               rel="noopener noreferrer"
+              className="text-blue-600 hover:underline"
             >
               gradsteps
             </a>{" "}
             (an automated degree planner), tjrides (a carpool platform for high
             school students), and electralert (a drowsy driving detection system
-            using eeg).
+            using eeg), and many many other{" "}
+            <a
+              href="https://github.com/tanmaik?tab=repositories"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-blue-600 hover:underline"
+            >
+              things
+            </a>
+            .
           </p>
         </div>
       </main>
