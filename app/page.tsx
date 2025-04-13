d0843c66a30803ec80f7de5a5f079705c810b742
Tanmai Kalisipudi <tanmai.kalisipudi@gmail.com>
2025-04-13 19:27:51 -0400

    make simple

--- app/page.tsx
+++ app/page.tsx
@@ -3,61 +3,44 @@ export default function Home() {
     <>
       <main className="flex items-center justify-center">
         <div className="w-full px-4 max-w-xl">
-          <h1 className="text-2xl font-bold mt-8">hey i&apos;m tanmai</h1>
+          <h1 className="text-2xl font-bold mt-14">tanmai kalisipudi</h1>
+
           <p className="mt-4">
-            currently, im a cs+math major at the university of virginia. i am
-            mainly interested in ai, startups, economics, and politics. you can
-            find me on{" "}
+            i&apos;m currently a CS+math at The University of Virginia. i&apos;m
+            very interested in AI and the future of work.
+          </p>
+          <p className="mt-2">
+            for more about my work:{" "}
             <a
+              className="text-blue-600 underline"
+              target="_blank"
               href="https://github.com/tanmaik"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="text-blue-600 hover:underline"
             >
-              github
-            </a>{" "}
-            and{" "}
-            <a
-              href="https://linkedin.com/in/tanmaikalisipudi"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="text-blue-600 hover:underline"
-            >
-              linkedin
+              GitHub
             </a>
-            .
-          </p>
-          <h2 className="text-xl font-bold mt-8">previously</h2>
-          <p className="mt-4">
-            in 2023, i was a software engineering intern at lineaje where i
-            spearheaded their flagship agentic llm product using langchain and
-            fine-tuned llms to improve instruction following.
-          </p>
-          <p className="mt-4">
-            before that, i interned at cisco systems where i worked on
-            optimizing shipping crate storage during the covid supply chain
-            crisis using cisco&apos;s meraki and nexus dashboard.
-          </p>
-          <p className="mt-4">
-            i&apos;ve also worked on several projects including{" "}
+            ,{" "}
             <a
-              href="https://gradsteps.com"
+              className="text-blue-600 underline"
               target="_blank"
-              rel="noopener noreferrer"
-              className="text-blue-600 hover:underline"
+              href="https://www.linkedin.com/in/tanmaikalisipudi"
             >
-              gradsteps
-            </a>{" "}
-            (an automated degree planner), tjrides (a carpool platform for high
-            school students), and electralert (a drowsy driving detection system
-            using eeg), and many many other{" "}
+              LinkedIn
+            </a>
+            ,{" "}
             <a
-              href="https://github.com/tanmaik?tab=repositories"
+              className="text-blue-600 underline"
               target="_blank"
-              rel="noopener noreferrer"
-              className="text-blue-600 hover:underline"
+              href="mailto:tanmai.kalisipudi@gmail.com"
             >
-              things
+              email
+            </a>
+            , and my{" "}
+            <a
+              className="text-blue-600 underline"
+              target="_blank"
+              href="https://drive.google.com/file/d/1yfiycfLNzf8jikumsP1ys43ga24byUye/view?usp=sharing"
+            >
+              resume
             </a>
             .
           </p>
