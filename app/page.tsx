d5e3142c00872f028aef24589ae4a746ec35f504
Tanmai Kalisipudi <tanmai.kalisipudi@gmail.com>
2025-05-14 02:30:26 -0400

    housekeeping

--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,10 @@ export default function Home() {
     <div className="max-w-[42rem] mx-auto mt-20 px-4">
       <h2 className="text-2xl font-bold mb-4">Tanmai Kalisipudi</h2>
       <p className="mb-4">
-        I am a computer science &amp; math student at The University of Virginia, and I&apos;m expecting to graduate by the spring of 2027. Before this, I attended Pitt (which I transferred from after my first year) and the Thomas Jefferson High School for Science and Technology.
+        I am a computer science &amp; math student at <a href="https://www.virginia.edu/" target="_blank" rel="noopener noreferrer" className="text-blue-600 underline" >The University of Virginia</a>, and I&apos;m expecting to graduate by the spring of 2027. Before this, I went to <a href="https://tjhsst.fcps.edu/" target="_blank" rel="noopener noreferrer" className="text-blue-600 underline" >TJHSST</a>.
       </p>
       <p className="mb-4">
-        I&apos;ve been programming since I was 5. I&apos;m mainly interested in AI, startups, economics and politics. If you&apos;re looking for my professional experience, here&apos;s my <a href="/resume" className="text-blue-600 underline" >resume</a>.
+        I&apos;ve been programming since I was 5. I&apos;m interested in AI, startups, economics and politics. If you&apos;re looking for my professional experience, here&apos;s my <a href="/resume" className="text-blue-600 underline" >resume</a>.
       </p>
     </div>
   );
