a2f3a5cc8a6814b184030e2155a951253927384c
Tanmai Kalisipudi <tanmai.kalisipudi@gmail.com>
2025-05-20 19:49:39 -0400

    add minecraft map

---
+++ app/minecraft/map/page.tsx
@@ -0,0 +1,10 @@
+export default function MinecraftMap() {
+  return (
+    <div style={{height: "100vh", width: "100%"}}>
+      <iframe 
+        src="http://54.236.196.60:8100" 
+        style={{border: "none", height: "100%", width: "100%"}} 
+      />
+    </div>
+  )
+}
