27ff3eda3ac2cef1fd11526a360e7d9241fc7fbd
Tanmai Kalisipudi <tanmai.kalisipudi@gmail.com>
2025-05-20 19:59:42 -0400

    try again

--- app/minecraft/map/page.tsx
+++ app/minecraft/map/page.tsx
@@ -1,9 +1,11 @@
+// pages/minecraft/map.js
 export default function MinecraftMap() {
   return (
     <div style={{height: "100vh", width: "100%"}}>
       <iframe 
-        src="http://minecraft.tanmai.org:8100" 
+        src="https://minecraft.tanmai.org" 
         style={{border: "none", height: "100%", width: "100%"}} 
+        allow="fullscreen"
       />
     </div>
   )
