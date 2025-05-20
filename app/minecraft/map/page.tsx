79d5ad8de1b9e9818fcfcda3a9f830988306da72
Tanmai Kalisipudi <tanmai.kalisipudi@gmail.com>
2025-05-20 19:50:02 -0400

    make easier link

--- app/minecraft/map/page.tsx
+++ app/minecraft/map/page.tsx
@@ -2,7 +2,7 @@ export default function MinecraftMap() {
   return (
     <div style={{height: "100vh", width: "100%"}}>
       <iframe 
-        src="http://54.236.196.60:8100" 
+        src="http://minecraft.tanmai.org:8100" 
         style={{border: "none", height: "100%", width: "100%"}} 
       />
     </div>
