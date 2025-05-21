227094f80e98da8413148ed35b8b5778b4b30433
Tanmai Kalisipudi <tanmai.kalisipudi@gmail.com>
2025-05-20 20:00:08 -0400

    nextc onfgi

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,19 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  async headers() {
+    return [
+      {
+        source: '/minecraft/map',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: "frame-src 'self' https://minecraft.tanmai.org;"
+          }
+        ]
+      }
+    ]
+  },
   devIndicators: false
 };
 
