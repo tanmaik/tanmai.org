e8b2fd28c5b44d49f4b0f6fc31ce1d10312ac7ea
Tanmai Kalisipudi <tanmai.kalisipudi@gmail.com>
2025-09-15 00:21:29 -0400

    simple

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,42 +1,5 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
 import "./globals.css";
-
-const iowaOldStyle = localFont({
-  src: [
-    {
-      path: "../public/fonts/bitstream-iowan-old-style-bt-586c36a8d7712.ttf",
-      weight: "400",
-      style: "normal",
-    },
-    {
-      path: "../public/fonts/bitstream-iowan-old-style-italic-bt-586c3740dc396.ttf",
-      weight: "400",
-      style: "italic",
-    },
-    {
-      path: "../public/fonts/bitstream-iowan-old-style-bold-bt-586c371d8d669.ttf",
-      weight: "700",
-      style: "normal",
-    },
-    {
-      path: "../public/fonts/bitstream-iowan-old-style-bold-italic-bt-586c37701cb62.ttf",
-      weight: "700",
-      style: "italic",
-    },
-    {
-      path: "../public/fonts/bitstream-iowan-old-style-black-bt-586c36e930225.ttf",
-      weight: "900",
-      style: "normal",
-    },
-    {
-      path: "../public/fonts/bitstream-iowan-old-style-black-italic-bt-586c378f12ca1.ttf",
-      weight: "900",
-      style: "italic",
-    },
-  ],
-  variable: "--font-iowa-old-style",
-});
 
 export const metadata: Metadata = {
   title: "Tanmai Kalisipudi",
@@ -49,7 +12,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${iowaOldStyle.variable}`}>{children}</body>
+      <body>{children}</body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,114 @@
-import { redirect } from 'next/navigation';
-
 export default function Home() {
-  redirect('https://github.com/tanmaik');
+  return (
+    <div
+      className="flex justify-center "
+      style={{ fontFamily: "Times New Roman, serif" }}
+    >
+      <div className="p-4 mt-8 sm:mt-16 max-w-xl">
+        <h1 className="font-semibold">Tanmai Kalisipudi</h1>
+        <div className="mt-4">
+          <p>
+            Hi, I&apos;m Tanmai! I&apos;m 20 years old and grew up in Northern
+            Virginia. I&apos;m currently studying math and CS at the University
+            of Virginia, and previously graduated from Thomas Jefferson High
+            School.
+          </p>
+          <p className="mt-2">
+            My passions include startups, AI, finance, government, and running.
+            I&apos;ve worked at{" "}
+            <a
+              href="https://www.cisco.com"
+              className="text-blue-600 visited:text-purple-800 underline"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Cisco
+            </a>
+            ,{" "}
+            <a
+              href="https://lineaje.com"
+              className="text-blue-600 visited:text-purple-800 underline"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Lineaje
+            </a>
+            , and{" "}
+            <a
+              href="https://www.suitable.co"
+              className="text-blue-600 visited:text-purple-800 underline"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Suitable
+            </a>
+            , and also created several businesses of my own.
+          </p>
+        </div>
+        <p className="mt-4">Some extracurriculars: </p>
+        <ul className="list-disc mt-2 pl-6 ">
+          <li>
+            Sold AI-powered{" "}
+            <a
+              href="https://gradsteps.com"
+              className="text-blue-600 visited:text-purple-800 underline"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              degree planning software
+            </a>{" "}
+            to the University of Pittsburgh
+          </li>
+          <li>
+            Built{" "}
+            <a
+              href="https://govey.org"
+              className="text-blue-600 visited:text-purple-800 underline"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              AI agents
+            </a>{" "}
+            that accelerate deal flow for defense contractors
+          </li>
+          <li>
+            Designed electroencephalography goggles to yell at sleepy drivers
+          </li>
+          <li>
+            Created a platform to help students from my{" "}
+            <a
+              href="https://tjhsst.fcps.edu"
+              className="text-blue-600 visited:text-purple-800 underline"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              high school
+            </a>{" "}
+            organize carpools
+          </li>
+
+          <li>
+            Organized 500+ hours of math and physics lessons in my private
+            tutoring business
+          </li>
+          <li>Hosted financial literacy webinars for high schoolers</li>
+          <li>
+            Founded a{" "}
+            <a
+              href="https://www.nest4us.org"
+              className="text-blue-600 visited:text-purple-800 underline"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              nonprofit
+            </a>{" "}
+            to teach young people to care for their local environment
+          </li>
+          <li>Ran sub-3:45 marathon and a sub-5:00 minute mile</li>
+          <li>(Somehow) attained a third-degree black belt in taekwondo</li>
+          <li>Been buying $META since 2022</li>
+        </ul>
+      </div>
+    </div>
+  );
 }
